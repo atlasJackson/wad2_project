f584f169d553d47d5f216cb98f25ca847b9cb2a0
nicolamoessner <nicola.moessner@gmail.com>
2018-03-07 10:50:25 +0000

    Create basic templates, url matching, and basic views.

--- fives/urls.py
+++ fives/urls.py
@@ -3,5 +3,8 @@ from fives import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    
+    url(r'^match_list/$', views.match_list, name='match_list'),
+    url(r'^create_match/$', views.create_match, name='create_match'),
+    url(r'^about_us/$', views.about_us, name='about_us'),
+
 ]
--- fives/views.py
+++ fives/views.py
@@ -6,3 +6,18 @@ def index(request):
     context_dict = {}
     response = render(request, 'fives/index.html', context=context_dict)
     return response
+
+def about_us(request):
+    context_dict = {}
+    response = render(request, 'fives/about_us.html', context=context_dict)
+    return response
+
+def match_list(request):
+    context_dict = {}
+    response = render(request, 'fives/match_list.html', context=context_dict)
+    return response
+
+def create_match(request):
+    context_dict = {}
+    response = render(request, 'fives/create_match.html', context=context_dict)
+    return response
