6defb4750d2c2b25b390724379c72817d0023c45
Gordon Daffurn <0801676d@student.gla.ac.uk>
2018-03-22 23:45:19 +0000

    Tidied up unit tests.

--- fives/tests.py
+++ fives/tests.py
@@ -6,18 +6,20 @@ from geopy.geocoders import Nominatim
 from datetime import datetime
 import uuid
 
-# Create your tests here.
+### Model Tests 
 class PlayerMethodTests(TestCase):
-    # Test the string representation of the player returns their username. 
+    # Test that the string representation of the player returns their username. 
     def test_player_string_representation(self):
         test_user, test_player = generate_test_user("test-user-1", "fivesPass123", "testemail@testmail.com", "Test", "User")
         self.assertEqual(str(test_player), test_user.username)
 
+# Helper method to return user and player objects.
 def generate_test_user(username, password, email, first_name, last_name):
     u = create_user(username, password, email, first_name, last_name)
     p = create_player(u)
     return u, p
 
+# Helper method to create a user object.
 def create_user(username, password, email, first_name, last_name):
     u = User.objects.get_or_create(username=username)[0]
     u.set_password(password)
@@ -27,6 +29,7 @@ def create_user(username, password, email, first_name, last_name):
     u.save()
     return u
 
+# Helper method to create a player object corresponding to the user passed in as a parameter.
 def create_player(user, gender=0, host_rating=0, num_host_ratings=0, punctuality=0, likeability=0, skill=0, num_player_ratings=0):
     u = User.objects.get_or_create(username=user)[0]
     p = Player.objects.get_or_create(user=u)[0]
@@ -41,11 +44,13 @@ def create_player(user, gender=0, host_rating=0, num_host_ratings=0, punctuality
     return p
 
 class GameMethodTests(TestCase):
+    # Test that the string representation of the game returns the concatenation of the host, start date and game id (auto-generated). 
     def test_game_string_representation(self):
         test_user, test_player = generate_test_user("test-user-1", "fivesPass123", "testemail@testmail.com", "Test", "User")
         test_game = create_game(0, 9, "2018-02-28 14:00", "2018-02-28 15:00", 1, "66 Bankhead Dr", "Edinburgh", "EH11 4EQ", 5, 1, test_user)
         self.assertEqual(str(test_game), str(test_game.host) + " " + str(test_game.start) + " " + str(test_game.game_id)[:6])
 
+# Helper method to create a game object.
 def create_game(game_type, free_slots, start, end, duration,
             street, city, postcode, price, booked, host):
 
@@ -68,3 +73,17 @@ def create_game(game_type, free_slots, start, end, duration,
     g.booked = booked
     g.save()
     return g
+
+class ParticipationMethodTests(TestCase):
+    # Test that the string representation of the participation object returns the concatenation of the player, game and rating.
+    def test_participation_string_representation(self):
+        test_user, test_player = generate_test_user("test-user-1", "fivesPass123", "testemail@testmail.com", "Test", "User")
+        test_game = create_game(0, 9, "2018-02-28 14:00", "2018-02-28 15:00", 1, "66 Bankhead Dr", "Edinburgh", "EH11 4EQ", 5, 1, test_user)
+        test_participation = create_participation(test_game, test_player, 0) 
+        self.assertEqual(str(test_participation), str(test_participation.player) + " " + str(test_participation.game) + " " + str(test_participation.rated))
+
+# Helper method to create a participation object.
+def create_participation(game, player, rated):
+    p = Participation(game=game, player=player, rated=rated)
+    p.save()
+    return p
